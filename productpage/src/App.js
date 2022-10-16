46e7450b3f64a6560745b876c67b7285377d2d16
Prithvika <98800028+prithvi2100@users.noreply.github.com>
2022-10-16 22:14:40 +0400

    Adding Banner

--- productpage/src/App.js
+++ productpage/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import Banner from "./components/banner";
 import Header from "./components/header";
 import Footer from "./components/footer";
 import MainContent from "./components/mainContent";
@@ -7,6 +8,7 @@ import MainContent from "./components/mainContent";
 function App() {
   return (
     <div className="container">
+      <Banner />
       <Header />
       <MainContent />
       <Footer />
